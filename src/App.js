31d185eb48b0959820b1a2fbe3db6a35644e89b1
AlvaroLuken <al5031@nyu.edu>
2022-01-24 16:13:12 -0800

    fe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { ethers } from 'ethers';
+const faucetJson = require("./utils/Faucet.json");
 
 function App() {
+  // pure JS
+  const { ethereum } = window;
+
+  if(!ethereum) {
+    console.log("You don't appear to have MM! Install MetaMask!");
+  } else {
+    console.log("Provider detected!", ethereum);
+  }
+
+  ethereum.request({ method: 'eth_requestAccounts' });
+
+  const provider = new ethers.providers.Web3Provider(ethereum);
+
+  const contractAddress = "0xc059E21c921AB5c00f5816bf70fA05Fc6257b679";
+  const faucetABI = faucetJson.abi;
+  const signer = provider.getSigner();
+
+  const faucet = new ethers.Contract(contractAddress, faucetABI, signer);
+
+  async function withdraw() {
+    const amountToWithdraw = ethers.utils.parseUnits(".1", "ether");
+    await faucet.withdraw(amountToWithdraw);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">Our Faucet!</div>
+      <button className="my-button" onClick={withdraw}>Withdraw!</button>
     </div>
   );
 }
